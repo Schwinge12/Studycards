310792738bdb8216fd7f3bf0281d3dcaf02e3fd3
Müller, Jan <jan.mueller.5@student.fhws.de>
2020-04-29 13:18:42 +0200

    - Unterklassen Student/Tutor/Dozent/Stapel erweitert KK

---
+++ lib/Speicherung/Stapel.dart
@@ -0,0 +1,13 @@
+import 'package:karteikartenapp/Speicherung/Karteikarte.dart';
+
+import 'Produkt.dart';
+
+class Stapel extends Produkt {
+//____________________________________Variables_______________________________
+ List<Karteikarte> stapelKarten;
+
+//____________________________________Constructor_____________________________
+  Stapel();
+//____________________________________Methods_________________________________
+
+}

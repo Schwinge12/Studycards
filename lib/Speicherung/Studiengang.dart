e87b4fb26f88172a02e3030eeeb1eae80cf7d7ce
Müller, Jan <jan.mueller.5@student.fhws.de>
2020-05-06 18:37:58 +0200

    Unterklasse Studiengang - einfügen in Userdata/Student/Karteikarte möglich

---
+++ lib/Speicherung/Studiengang.dart
@@ -0,0 +1,18 @@
+import 'Produkt.dart';
+
+class Studiengang extends Produkt {
+
+//____________________________________Variables_______________________________
+  String name;
+
+//____________________________________Constructor_____________________________
+  Studiengang(String name){
+    this.name = name;
+
+  }
+
+//____________________________________Methods_________________________________
+
+
+
+}

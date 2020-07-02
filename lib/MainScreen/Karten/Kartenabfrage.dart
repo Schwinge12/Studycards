07c25c9274fc73ae59e6d75886ce51a28d098f27
Müller, Jan <jan.mueller.5@student.fhws.de>
2020-07-02 19:02:30 +0200

    Array out of Range fix

--- lib/MainScreen/Karten/Kartenabfrage.dart
+++ lib/MainScreen/Karten/Kartenabfrage.dart
@@ -85,7 +85,8 @@ class _KartenanzeigeState extends State<Kartenabfrage> {
                                             Expanded(
                                               child: Container(
                                                   color: Colors.white,
-                                                  child: new Center(child: widget.stapel.stapelKarten[widget.kartennummer].bilder[0] == null
+                                                  child:
+                                                  new Center(child: widget.stapel.stapelKarten[widget.kartennummer].bilder.length == 0
                                                       ? new Text("Kein Bild vorhanden.")
                                                       : new Image.file(widget.stapel.stapelKarten[widget.kartennummer].bilder[0], width: 400, height: 400))
                                               ),
@@ -131,7 +132,7 @@ class _KartenanzeigeState extends State<Kartenabfrage> {
                                             Expanded(
                                               child: Container(
                                                   color: Colors.white,
-                                                  child: new Center(child: widget.stapel.stapelKarten[widget.kartennummer].bilder[0] == null
+                                                  child: new Center(child: widget.stapel.stapelKarten[widget.kartennummer].bilder.length < 2
                                                       ? new Text("Kein Bild vorhanden.")
                                                       : new Image.file(widget.stapel.stapelKarten[widget.kartennummer].bilder[0], width: 400, height: 400))
                                               ),
